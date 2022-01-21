685b55d45795681a29e8883bd4c9663b0cf73de2
speae <speae147258@daum.net>
2022-01-22 03:18:04 +0900

    CarExample update
    Merge remote-tracking branch 'origin/main' into main

--- This_is_Java/src/chapter6/Car.java
+++ This_is_Java/src/chapter6/Car.java
@@ -2,14 +2,11 @@ package chapter6;
 
 public class Car {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String company = "현대자동차";
-		String model = "그랜저";
-		String color = "검정";
-		int maxSpeed = 350;
-		int speed;
-		
-	}
+	// TODO Auto-generated method stub
+	String company = "현대자동차";
+	String model = "그랜저";
+	String color = "검정";
+	int maxSpeed = 350;
+	int speed;
 
 }
